e1c870c27591067c081cd0c00f89f00c58826aff
wbrunovieira <wbrunovieira77@gmail.com>
2024-02-09 21:58:20 -0300

    added full calendar

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import './App.css';
 
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+
 function App() {
   return (
     <>
       <h1>Tarefas Diarias</h1>
+      <FullCalendar
+        plugins={[dayGridPlugin, interactionPlugin, listPlugin]}
+        initialView='dayGridMonth'
+        navLinks={true}
+        selectable={true}
+        editable={true}
+        events={[
+          { title: 'Ler', date: '2024-02-01' },
+          { title: 'Estudar', date: '2024-02-02' },
+        ]}
+      />
     </>
   );
 }
